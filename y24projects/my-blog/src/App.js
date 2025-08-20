acf6de7c816369f5efb33763472a806e13e694b9
nagavenkatmelagiri <nagavenkat205@gmail.com>
2025-08-20 22:20:20 +0530

    expert 2

--- y24projects/my-blog/src/App.js
+++ y24projects/my-blog/src/App.js
@@ -1,12 +1,20 @@
-
 import './App.css';
+
 function App() {
+  const myStr = "Hello";
+  const a = 2.5;
+  const myLink = "https://newerp.kluniversity.in/";
+
   return (
     <div className="App">
       <div className="content">
-        Hello World
+        <h1>Hello World</h1>
+        <h1>{myStr}</h1>
+        <h1>{a}</h1>
+        <a href={myLink}>My Link</a>
       </div>
     </div>
   );
 }
+
 export default App;
