b2338c4bcb29785cdf5a9218485ef6b052fd6f0d
nagavenkatmelagiri <nagavenkat205@gmail.com>
2025-08-21 18:21:29 +0530

    exp12

--- y24projects/my-blog/src/Home.js
+++ y24projects/my-blog/src/Home.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import BlogList from './BlogList';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
+    const [blogs, ] = useState([
         { title: "Salaar", body: "Prabhas", author: "Neel", id: 1 },
         { title: "RRR", body: "NTR", author: "Rajamouli", id: 2 },
         { title: "Athadu", body: "Mahesh Babu", author: "Trivikram", id: 3 }
@@ -11,6 +11,7 @@ const Home = () => {
     return (
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs!" />
+            <BlogList blogs = {blogs.filter((blog) => blog.author === "Venky")} title="Venky's blogs" />
         </div>
     );
 };
