d1a9e4ef3d4a562ad7f98d95b8cd422c01b62951
nagavenkatmelagiri <nagavenkat205@gmail.com>
2025-08-21 09:04:33 +0530

    exp7 ex1

--- y24projects/my-blog/src/Home.js
+++ y24projects/my-blog/src/Home.js
@@ -1,8 +1,12 @@
 const Home = () => {
+    const handleClick = () => {
+        console.log("hello everyone");
+    };
     return (
         <div className="home">
             <h2>Welcome to My Blog</h2>
-            <h3>Latest Posts</h3>
+            <button onClick={handleClick}>Click Me</button>
+            <h3>Latest Posts</h3>   
         </div>
     );
 }
