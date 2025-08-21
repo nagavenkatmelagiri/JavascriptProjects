27ed3d8020dbe6a2f7bfe2265b531b228e97d1a1
nagavenkatmelagiri <nagavenkat205@gmail.com>
2025-08-21 20:14:27 +0530

    exp 15 & 16

--- y24projects/my-blog/src/Home.js
+++ y24projects/my-blog/src/Home.js
@@ -1,24 +1,18 @@
-import { useState , useEffect } from "react";
+import { useState, useEffect } from "react";
 import BlogList from './BlogList';
 
 const Home = () => {
-    const [blogs,setBlogs] = useState([
-        { title: "Salaar", body: "Prabhas", author: "Neel", id: 1 },
-        { title: "RRR", body: "NTR", author: "Rajamouli", id: 2 },
-        { title: "Athadu", body: "Mahesh Babu", author: "Trivikram", id: 3 }
-    ]);
-const handleDelete = (id) => {
-    const newBlogs = blogs.filter(blog => blog.id !== id);
-    setBlogs(newBlogs);
-}
-useEffect(() => {
-    console.log('use effect ran');
-    console.log(blogs);
-},[]);
+    const [blogs, setBlogs] = useState(null);
+
+    useEffect(() => {
+        fetch('http://localhost:8000/blogs')
+            .then(res => res.json())
+            .then(data => setBlogs(data));
+    }, []);
+
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
-            <BlogList blogs = {blogs.filter((blog) => blog.author === "Venky")} title="Venky's blogs" />
+            {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
         </div>
     );
 };
