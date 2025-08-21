2e16679b0dc63a56c2a1654e1a2ac4938c0ecdd9
nagavenkatmelagiri <nagavenkat205@gmail.com>
2025-08-21 19:24:41 +0530

    exp14

--- y24projects/my-blog/src/Home.js
+++ y24projects/my-blog/src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState , useEffect } from "react";
 import BlogList from './BlogList';
 
 const Home = () => {
@@ -10,7 +10,11 @@ const Home = () => {
 const handleDelete = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
     setBlogs(newBlogs);
-};
+}
+useEffect(() => {
+    console.log('use effect ran');
+    console.log(blogs);
+},[]);
     return (
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
